099df1b60fc0a720f52ef67bce03d350a0cc3328
Sigrsaeli <m.nikita.d@gmail.com>
2020-10-09 22:08:07 +0300

    Update 1_homework_squirrel.py

--- 1_homework_squirrel.py
+++ 1_homework_squirrel.py
@@ -3,10 +3,8 @@ def factorial(n):
     if n == 1:
         return 1
     if n == 0:
-        return 0
+        return 1
     return n*factorial(n-1)
 
 def squirrel (n):
-    if n == 0:
-        return 0
     return int(str(factorial(n=n))[0])
