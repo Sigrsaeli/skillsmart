1c91f254d3fa6e46ec61eb2c63fb2fa107cf3c1c
Sigrsaeli <m.nikita.d@gmail.com>
2020-10-14 16:32:04 +0300

    Update 2_homework.py

--- 2_homework.py
+++ 2_homework.py
@@ -1,1 +1,9 @@
 
+def odometer(N):
+    sum = 0
+    for i in range(len(N)):
+        if i == 1:
+            sum += N[i]*N[i-1]
+        elif (i % 2 !=0) and (i != 1):
+            sum += N[i-1]*(N[i] - N[i-2])
+    return sum
