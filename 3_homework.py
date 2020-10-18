3fd460c2c251cf22db9b8df4d8b9bf44f46ea0ba
Sigrsaeli <m.nikita.d@gmail.com>
2020-10-18 19:59:09 +0300

    Update 3_homework.py

--- 3_homework.py
+++ 3_homework.py
@@ -1,1 +1,18 @@
 
+
+def ConquestCampaign (N, M, L, battalion):
+    days = 0
+    areas = [[0 for j in range(M)] for i in range(N)]
+    for i in range(len(battalion)):
+        if i % 2 == 0:
+            areas[battalion[i]-1][battalion[i+1]-1] = 1
+            days = 1
+    while sum([sum(areas[i]) for i in range(N)]) != N*M:
+        for i in range(N):
+            for j in range(M):
+                if (areas[i][j] == 0) and ((areas[i-1][j] == 1) or (areas[i+1][j] == 1) or (areas[i][j-1] == 1) or (areas[i][j+1] == 1)):
+                    areas[i][j] = 1
+                if (i == N-1) and (j == M-1):
+                    days += 1
+
+    return days
