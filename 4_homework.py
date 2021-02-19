b169f0f6f25f805e04163e1fbb3dae3ccc44a99e
Sigrsaeli <m.nikita.d@gmail.com>
2021-02-19 14:25:44 +0300

    Update 4_homework.py
    First try for this homework

--- 4_homework.py
+++ 4_homework.py
@@ -1,1 +1,12 @@
 
+def MadMax(N, Tele):    
+    Tele.sort()
+    max_num = max(Tele)
+    max_index = Tele.index(max_num)
+    median_index = (len(Tele)//2)
+    Tele.pop(max_index)
+    Tele.insert(median_index, max_num)
+    Tele[median_index+1:len(Tele)].sort()
+    Tele[median_index+1:len(Tele)] = sorted(Tele[median_index+1:len(Tele)], reverse=True)
+
+    return Tele
