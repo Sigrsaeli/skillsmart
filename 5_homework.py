c1dbbd93ee728e5ab27ec3dba423fac6b0066037
Sigrsaeli <m.nikita.d@gmail.com>
2021-02-21 02:16:46 +0300

    Update 5_homework.py
    First version of homework

--- 5_homework.py
+++ 5_homework.py
@@ -1,1 +1,9 @@
 
+def SynchronizingTables(N, ids, salary):
+    new_ids=sorted(ids)
+    new_salary=sorted (salary)
+    connect_dict=dict(zip(new_ids,new_salary))
+    return_salary=[]
+    return_salary.append(connect_dict[item] for item in ids)
+    for i in return_salary:
+        return list(i)
