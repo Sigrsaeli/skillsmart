e91a082e4e482a5573f304ab4f583ad46a2fe66a
Sigrsaeli <m.nikita.d@gmail.com>
2021-02-22 23:36:23 +0300

    Update 6_homework.py
    ',' changed for '.'

--- 6_homework.py
+++ 6_homework.py
@@ -2,21 +2,21 @@
 def PatternUnlock(N, hits):
     dist_len = 0
     if (set([1,5]).issubset(hits)) or (set([5,1]).issubset(hits)):
-        dist_len += 1,4142135
+        dist_len += 1.4142135
     elif (set([9,2]).issubset(hits)) or (set([2,9]).issubset(hits)):
-        dist_len += 1,4142135
+        dist_len += 1.4142135
     elif (set([4,2]).issubset(hits)) or (set([2,4]).issubset(hits)):
-        dist_len += 1,4142135
+        dist_len += 1.4142135
     elif (set([8,3]).issubset(hits)) or (set([3,8]).issubset(hits)):
-        dist_len += 1,4142135
+        dist_len += 1.4142135
     elif (set([3,5]).issubset(hits)) or (set([5,3]).issubset(hits)):
-        dist_len += 1,4142135
+        dist_len += 1.4142135
     elif (set([6,2]).issubset(hits)) or (set([2,6]).issubset(hits)):
-        dist_len += 1,4142135
+        dist_len += 1.4142135
     elif (set([7,2]).issubset(hits)) or (set([2,7]).issubset(hits)):
-        dist_len += 1,4142135
+        dist_len += 1.4142135
     elif (set([8,1]).issubset(hits)) or (set([1,8]).issubset(hits)):
-        dist_len += 1,4142135
+        dist_len += 1.4142135
     elif (set([9,1]).issubset(hits)) or (set([1,9]).issubset(hits)):
         dist_len += 1
     elif (set([6,1]).issubset(hits)) or (set([1,6]).issubset(hits)):
